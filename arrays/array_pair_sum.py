cb8017f82e61bcf2fa14e3606f945dd9a2caaaff
Keith Morimoto <seatown1two@hotmail.com>
2018-11-17 14:28:12 -0800

    Fixed indents.

--- arrays/array_pair_sum.py
+++ arrays/array_pair_sum.py
@@ -1,16 +1,16 @@
 def pair_sum(array, k):
-	array.sort()
-	i = 0
-	j = len(array) - 1
-	tuples = []
+    array.sort()
+    i = 0
+    j = len(array) - 1
+    tuples = []
 
-	while i < j:
-		total = array[i] + array[j]
-		if total > k:
-			j -= 1
-		elif total < k:
-			i += 1
-		elif total == k:
-			tuples.append((array[i], array[j]))
-			i += 1
-	return tuples
+    while i < j:
+        total = array[i] + array[j]
+        if total > k:
+            j -= 1
+        elif total < k:
+            i += 1
+        elif total == k:
+            tuples.append((array[i], array[j]))
+            i += 1
+            return tuples
