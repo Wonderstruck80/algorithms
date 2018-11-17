d8a56d9157258932870573e0d975549bf1844f42
Keith Morimoto <seatown1two@hotmail.com>
2018-11-17 13:03:31 -0800

    Completed array_pair_sum.

--- arrays/__init__.py
+++ arrays/__init__.py
@@ -1,3 +1,1 @@
-from os.path import basename, listdir
-
-__all__ = [basename(f)[:-3] for f in listdir(dirname(__file__)) if f.endswith('.py') and f != '__init__.py']
+from .array_pair_sum import *
--- arrays/array_pair_sum.py
+++ arrays/array_pair_sum.py
@@ -1,2 +1,16 @@
-def sumnums(x, y):
-	return x + y
+def pair_sum(array, k):
+	array.sort()
+	i = 0
+	j = len(array) - 1
+	tuples = []
+
+	while i < j:
+		total = array[i] + array[j]
+		if total > k:
+			j -= 1
+		elif total < k:
+			i += 1
+		elif total == k:
+			tuples.append((array[i], array[j]))
+			i += 1
+	return tuples
